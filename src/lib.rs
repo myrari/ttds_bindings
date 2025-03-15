d846fe6d2d2a2922964cf02f31911390ff12c186
myrari <myra@myrari.net>
2025-03-15 14:39:16 -0700

    implemented get_token method with error

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 pub struct Color {
     pub r: u8,
     pub g: u8,
-    pub b: u8
+    pub b: u8,
 }
 
 #[derive(Debug)]
@@ -10,6 +10,16 @@ pub struct Pane {
     pub name: String,
     pub bg_color: Color,
     //pub conn: Connection,
-    
     token: Option<String>,
+}
+
+impl Pane {
+    pub fn get_token(&self) -> Result<String, String> {
+        return match &self.token {
+            Some(token) => Ok(token.clone()),
+            None => Err(String::from(
+                "Token not yet initialized! Did you succesfully open a connection?",
+            )),
+        };
+    }
 }
