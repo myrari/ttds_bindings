415651ba282c2c60d2a5aa14991f91820a269eec
Ian <zzarlow@gmail.com>
2025-03-15 14:53:15 -0700

    Began implementing the `Connection` struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 pub struct Color {
     pub r: u8,
@@ -12,4 +14,28 @@ pub struct Pane {
     //pub conn: Connection,
     
     token: Option<String>,
+}
+
+pub struct Connection {
+    host: String,
+    use_tls: bool,
+}
+
+impl Connection {
+    pub fn new(host: impl Into<String>, use_tls: bool) -> Self {
+        Self {
+            host: host.into(),
+            use_tls,
+        }
+    }
+
+    pub fn request(
+        self,
+        args: &[String], // TODO: Find better name for this
+        method: Option<&str>,
+        auth: Option<&str>,
+        kw_args: HashMap<String, String>, // TODO: Find better name for this
+    ) -> String {
+        todo!()
+    }
 }
